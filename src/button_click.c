71da35ddaf08b3d38f80189d1ed7032efa7cc58d
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2015-11-08 22:44:05 +0100

    Tile redirection data finished

--- src/button_click.c
+++ src/button_click.c
@@ -40,8 +40,23 @@ int bitmapcnt=0;
 
 // Animation Timer
 AppTimer *timer;
-const int delta = 250;
-static int xxx=0;
+const int delta = 500;
+
+#define starttile 73
+
+// Tile redirection array (do not morph 0, normal tiles start at 1)
+static int redirect_tile[81] = { 0, 
+															  1,  9, 17, 25,  1, 33, 41, 25,			// Vertical
+															  2, 10, 18, 26,  2, 34, 42, 26,			// Horizontal
+																3, 11, 19, 27,  3, 35, 43, 27,      // Bottom Right
+																4, 12, 20, 28,  4, 36, 44, 28,      // Bottom Left
+																5, 13, 21, 29,  5, 37, 45, 29,      // Top Right
+																6, 14, 22, 30,  6, 38, 46, 30,      // Top Left
+																7, 15, 23, 31,  7, 39, 47, 31,      // Crossing Left To Right Empty Vert
+															 54, 51, 59, 61, 54, 52, 60, 61,      // Crossing Left to Right Full Vert
+															  7, 63, 63, 54,  7, 55, 55, 54,      // Crossing Top to Bottom Empty Horizontal
+															 31, 53, 53, 61, 31, 62, 62, 61       // Crossing Top to Bottom Full Horizontal
+															 };
 
 // Garbage collected sprite creation (reference counter only)
 static GBitmap* makeSprite(const GBitmap * base_bitmap, GRect sub_rect)
@@ -68,8 +83,12 @@ static int comparescore()
 //---------------------------------------------------------------------------------//
 
 void timer_callback(void *data) {
-		// Movement!
-		xxx++;
+	
+		// Simple float code
+		unsigned char ctile=tilemap[(cpy*ENGINE_TILEMAPWIDTH)+cpx];
+		ctile ++;
+		if(ctile>80) ctile=73;
+		tilemap[(cpy*ENGINE_TILEMAPWIDTH)+cpx]=ctile;
 	
 	  layer_mark_dirty(s_image_layer);
 
@@ -118,7 +137,7 @@ static void initGameBoard()
 	pdx=ENGINE_PLAYER_STARTDX;
 	pdy=ENGINE_PLAYER_STARTDY;
 
-	tilemap[(ENGINE_TILEMAPWIDTH*cpy)+cpx]=1;	
+	tilemap[(ENGINE_TILEMAPWIDTH*cpy)+cpx]=starttile;	
 	
 	// Randomize mines
 	/*
@@ -274,6 +293,9 @@ static void layer_update_callback(Layer *layer, GContext* ctx) {
 				ccx=cx;
 				for(int i=0;i<ENGINE_TILE_NOX;i++){
 						tileno=tilemap[(cy*ENGINE_TILEMAPWIDTH)+ccx];
+	
+						// Morph tile number to graphical tile representation!
+						tileno=redirect_tile[tileno];
 
 						// Draw Player
 						if((ccx==px)&&(cy==py)) tileno=8;
@@ -321,7 +343,7 @@ static void main_window_load(Window *window) {
 	// Define tiles (2 rows of 12 tiles at 12x12 px and 1 row of 14x23)
 
 	// ENGINE_TILE_NOX is used to count number of tiles automatically ENGINE_TILE_SIZE is used to size the tiles
-	for(int j=0;j<5;j++){		
+	for(int j=0;j<8;j++){		
 			for(int i=0;i<ENGINE_TILE_NOX;i++){
 					t_image[(j*ENGINE_TILE_NOX)+i] = makeSprite(s_image, GRect((i%ENGINE_TILE_SIZE)*ENGINE_TILE_SIZE, j*ENGINE_TILE_SIZE, ENGINE_TILE_SIZE, ENGINE_TILE_SIZE));			
 					
